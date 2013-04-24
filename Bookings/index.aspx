c2bd5937d2e8b9e9d9a3f33f7f87912d084517f3
Ubaid Khan <ubaidkhan88@gmail.com>
2013-04-24 21:24:14 +0300

    done changes to booking index

--- Bookings/index.aspx
+++ Bookings/index.aspx
@@ -6,62 +6,11 @@
   <div id="hotelsList" style="width:100%;">
     <h3 class="custom-h3 blue-font left">My Bookings</h3>
     <div class="clear"></div>
-    <asp:Repeater ID="rptrBookingIndex" runat="server">
-      <HeaderTemplate>
-       <div class="bookings">
-      </HeaderTemplate>
-      <ItemTemplate>
-        <div class="block-div">
-          <div class="block-content left" style="width:710px;">
-            <h3><%# Eval("name")%></h3>
-            <img src="<%# Eval("starsImagePath")%>" />
-            <h4><%# Eval("city") %> <%# Eval("country") %></h4>
-            <div class="margin10"></div>
-            <h4>Stay: <%# Eval("stay") %></h4>
-            <h4>Room: <%# Eval("room") %></h4>
-            <h4>Guest: <%# Eval("guests") %></h4>
-            <h4>Total Price SR.<%# Eval("totalPrice") %></h4>
-          </div>      
-          <div class="hotel-image right">
-            <div class="price">
-              SR. <%# Eval("pricePerPassenger")%> / pax
-            </div>
-            <img src="<%# Eval("defaultImagePath")%>" alt="" class="media-image" style="width:200px; height:200px" />
-          </div>
-          <div class="clear"></div>
-          <div class="margin10"></div>
-          <h3 class="custom-h3 blue-font">Cancellation Policy</h3>
-          <div class="div-default div-bordered">
-            Cancellation Policy Text
-          </div>
-          <div class="margin10"></div>
-          <h3>Guest Details</h3>
-          <asp:Repeater ID="rptrGuests" runat="server" DataSource='<%# Eval("guestDetails") %>'>
-            <HeaderTemplate>
-              <table class="table table-bordered">
-                <tr>
-                  <th>Guest Name</th>
-                </tr>
-            </HeaderTemplate>
-            <ItemTemplate>
-              <tr>
-                <td><%# Eval("name") %></td>
-              </tr>
-            </ItemTemplate>
-            <FooterTemplate>
-              </table>
-            </FooterTemplate>
-          </asp:Repeater>
-        </div>
-      </ItemTemplate>
-      <SeparatorTemplate>
-        <hr />
-        <div class="margin10"></div>
-      </SeparatorTemplate>
-      <FooterTemplate>
-        </div>
-      </FooterTemplate>
-    </asp:Repeater>
+    <asp:GridView ID="gvBookings" runat="server" 
+          onpageindexchanging="gvBookings_PageIndexChanging" 
+          onrowdatabound="gvBookings_RowDataBound" Width="100%" 
+          CssClass="table table-bordered" AllowPaging="True" PageSize="10">
+    </asp:GridView>
   </div>
   <div class="clear"></div>
 </asp:Content>
--- Bookings/index.aspx.cs
+++ Bookings/index.aspx.cs
@@ -5,13 +5,68 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using meis007Model;
+using System.Data;
 
 public partial class Bookings_index : System.Web.UI.Page
 {
+    meis007Entities _meis007Entities;
     protected void Page_Load(object sender, EventArgs e)
     {
-        var data = BookingsIndexObjectHelper.GetBookings();
-        rptrBookingIndex.DataSource = data;
-        rptrBookingIndex.DataBind();
+        if (!IsPostBack) {
+            BindDataToGridView();
+        }
+    }
+
+
+    protected void gvBookings_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        gvBookings.PageIndex = e.NewPageIndex;
+        BindDataToGridView();
+    }
+
+    protected void gvBookings_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType != DataControlRowType.DataRow) return;
+
+        LinkButton lb;
+        lb = new LinkButton();
+        lb.CommandArgument = e.Row.Cells[5].Text;
+        lb.CommandName = "NumClick";
+        lb.Text = "Details";
+        lb.PostBackUrl = "show.aspx?id=" + e.Row.Cells[5].Text;
+        e.Row.Cells[5].Controls.Add((Control)lb);
+    }
+
+    protected void BindDataToGridView()
+    {
+        var userId = CurrentUser.Id();
+        _meis007Entities = new meis007Entities();
+        var data = from pm in _meis007Entities.ProductMasters
+                   join bm in _meis007Entities.BkgMasters
+                   on pm.ProductID equals bm.ProductID
+                   where bm.ProductID != null && bm.CustConsultantID == userId
+                   orderby bm.BkgID descending
+                   select new { booking = bm, HotelName = pm.ProductName };
+        DataTable dt = new DataTable();
+        DataRow dr = null;
+        dt.Columns.Add(new DataColumn("Booking Date", typeof(string)));
+        dt.Columns.Add(new DataColumn("Lead Passenger Name", typeof(string)));
+        dt.Columns.Add(new DataColumn("Hotel", typeof(string)));
+        dt.Columns.Add(new DataColumn("Check In", typeof(string)));
+        dt.Columns.Add(new DataColumn("Check Out", typeof(string)));
+        dt.Columns.Add(new DataColumn("Details", typeof(string)));
+        foreach (var x in data)
+        {
+            dr = dt.NewRow();
+            dr["Booking Date"] = DateTimeHelper.ConvertToString(x.booking.BkgDate.ToString());
+            dr["Lead Passenger Name"] = x.booking.LeadPaxName;
+            dr["Hotel"] = x.HotelName;
+            dr["Check In"] = DateTimeHelper.ConvertToString(x.booking.CheckIN.ToString());
+            dr["Check Out"] = DateTimeHelper.ConvertToString(x.booking.checkOUT.ToString());
+            dr["Details"] = x.booking.BkgID;
+            dt.Rows.Add(dr);
+        }
+        gvBookings.DataSource = dt;
+        gvBookings.DataBind();
     }
 }
