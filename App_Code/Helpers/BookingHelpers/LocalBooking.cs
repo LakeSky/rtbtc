d3fa2de261b5a9db475914325cdd5f1766847a8c
Ubaid Khan <ubaidkhan88@gmail.com>
2013-05-20 14:16:52 +0300

    refactore few stuff

---
+++ App_Code/Helpers/BookingHelpers/LocalBooking.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using meis007Model;
+
+public class LocalBooking
+{
+  BkgMaster bkgMaster;
+  meis007Entities _meis007Entities;
+  SuppliersHotelsInfo Shi;
+  BasketHotelDetails Bhd;
+  PaxDetail paxDetail;
+  HotelBooking hotelBooking;
+  long BasketSequence;
+  string SupplierRefNo;
+  string SupplierConfNo;
+  string Name;
+  string BookingType;
+	public LocalBooking(meis007Entities obj1, SuppliersHotelsInfo obj2, BasketHotelDetails obj3, HotelBooking obj4, long basketSequence, string refNo, string confNo, string name, string type)
+	{
+    this.bkgMaster = new BkgMaster();
+    this._meis007Entities = obj1;
+    this.Shi = obj2;
+    this.Bhd = obj3;
+    this.BasketSequence = basketSequence;
+    this.SupplierRefNo = refNo;
+    this.SupplierConfNo = confNo;
+    this.Name = name;
+    this.hotelBooking = obj4;
+    this.BookingType = type;
+	}
+
+  public void Create() {
+    if (BookingType == "Hotel")
+    {
+      CreateHotelBooking();
+    }
+    else { 
+    }
+  }
+
+  public void CreateHotelBooking() {
+    bkgMaster = new BkgMaster
+    {
+      BasketID = BasketSequence,
+      SearchID = int.Parse(Shi.SearchID.ToString()),
+      HotelInfoID = Shi.HotelInfoID,
+      BkgDate = DateTime.Now,
+      BkgType = DbParameter.GetBookingType(),
+      BkgStatus = DbParameter.GetBookingStatus(),
+      CustomerID = 1015,
+      //DbParameter.GetCustomerId
+      CustConsultantID = CurrentUser.Id(),
+      CustomerDisplayPrice = Shi.LCAP,
+      CustomerMarkup = Shi.MarkUp,
+      FCSalesAmt = Shi.LCAP,
+      FCurrencyID = Shi.CurrencyCode,
+      BaseCurrencyID = ApplicationObject.GetBaseCurrency(),
+      ExRate = Shi.ExchRate,
+      SalesAmt = Shi.LCAP,
+      SupplierID = long.Parse(Shi.SupplierID.ToString()),
+      SupplierRefNo = SupplierRefNo,
+      SupplierConfNo = SupplierConfNo,
+      PayCurrID = Shi.CurrencyCode,
+      PayExRate = Shi.ExchRate,
+      PayAmt = Shi.FCAP,
+      ServiceName = DbParameter.GetServiceName(1),
+      CheckIN = Bhd.fromDate,
+      checkOUT = Bhd.toDate,
+      NoOfNights = Shi.NumOfNights,
+      ProductID = Shi.LocHotelID,
+      LeadPaxName = Name,
+      NoOfRooms = 1,
+      Adults = Bhd.guestDetails.Where(y => y.type == "Adult").Count(),
+      Children = Bhd.guestDetails.Where(y => y.type == "Kid").Count(),
+      Infants = Bhd.guestDetails.Where(y => y.type == "Infant").Count(),
+      HasExtraPaxs = true,
+      RateSDID = Shi.RateSDID,
+      supplierHotelID = int.Parse(Shi.HotelID.ToString()),
+      StarsLevel = Shi.StarsLevel,
+      SupplierRoomID = Shi.RoomID,
+      SupplierRoomName = Shi.RoomName,
+      SupplierRoomType = Shi.RoomType,
+      SupplierRoomTypeID = Shi.RoomTypeID,
+      BBID = Shi.BBID,
+      BBName = Shi.BBName,
+      ModDate = DateTime.Now,
+      UserID = CurrentUser.Id().ToString()
+    };
+    _meis007Entities.AddToBkgMasters(bkgMaster);
+    _meis007Entities.SaveChanges();
+    foreach (var y in Bhd.guestDetails)
+    {
+      paxDetail = new PaxDetail
+      {
+        BkgID = bkgMaster.BkgID,
+        PaxName = Name
+      };
+      _meis007Entities.AddToPaxDetails(paxDetail);
+      _meis007Entities.SaveChanges();
+    }
+    BkgCancelPolicyHelper bkgCancelPolicyHelper =
+        new BkgCancelPolicyHelper
+        {
+          _meis007Entities = _meis007Entities,
+          bkgMaster = bkgMaster,
+          EndDate = Bhd.toDate.ToString(),
+          hotelBooking = hotelBooking,
+          StartDate = Bhd.fromDate.ToString(),
+          SupplierName = Bhd.SupplierName,
+          suppliersHotelsInfo = Shi
+        };
+    bkgCancelPolicyHelper.CreateBkgCancelPolicies();
+  }
+}
--- Orders/checkout.aspx.cs
+++ Orders/checkout.aspx.cs
@@ -35,133 +35,34 @@ public partial class Orders_checkout : PublicApplicationPage
         _meis007Entities = new meis007Entities();
         var sequence = _meis007Entities.BasketSequences.OrderBy(x => x.Id).First();
         var basketSequence = sequence.SequenceNumber + 1;
-        var bookedHotels = false;
+        sequence.SequenceNumber = basketSequence;
+        _meis007Entities.SaveChanges();
         SuppliersHotelsInfo suppliersHotelsInfo;
-        BkgMaster bkgMaster;
-        PaxDetail paxDetail;
         basketHelper = GetBasketHelperObject();
         var hotelsToRemove = new List<BasketHotelDetails>();
+        SupplierBooking supplierBooking;
+        LocalBooking localBooking;
+        string name;
         foreach (var x in basketHelper.hotelDetails) {
             if (!DbParameter.IsInetrnalSupplier(x.supplierId)) {
-                bookedHotels = true;
                 suppliersHotelsInfo = _meis007Entities.SuppliersHotelsInfoes.Where(y => y.HotelInfoID == x.hotelInfoId).First();
-                string reservartionId =  CreateBooking(x, suppliersHotelsInfo);
+                supplierBooking = new SupplierBooking(suppliersHotelsInfo, x, txtFirstName.Text, txtMiddleName.Text, txtLastName.Text, txtMobile.Text, txtTelephone.Text);
+                string reservartionId = supplierBooking.Book();
                 var obj = _meis007Entities.HotelBookings.Where(y => y.HotelInfoId == x.hotelInfoId).FirstOrDefault();
                 if (obj != null)
                 {
+                    name = txtFirstName.Text + " " + txtMiddleName.Text + " " + txtLastName.Text;
                     var array = obj.ReservationId.Split('~');
-                    bkgMaster = new BkgMaster
-                    {
-                        BasketID = basketSequence,
-                        SearchID = int.Parse(suppliersHotelsInfo.SearchID.ToString()),
-                        HotelInfoID = suppliersHotelsInfo.HotelInfoID,
-                        BkgDate = DateTime.Now,
-                        BkgType = DbParameter.GetBookingType(),
-                        BkgStatus = DbParameter.GetBookingStatus(),
-                        CustomerID = 1015,
-                        //DbParameter.GetCustomerId
-                        CustConsultantID = CurrentUser.Id(),
-                        CustomerDisplayPrice = suppliersHotelsInfo.LCAP,
-                        CustomerMarkup = suppliersHotelsInfo.MarkUp,
-                        FCSalesAmt = suppliersHotelsInfo.LCAP,
-                        FCurrencyID = suppliersHotelsInfo.CurrencyCode,
-                        BaseCurrencyID = ApplicationObject.GetBaseCurrency(),
-                        ExRate = suppliersHotelsInfo.ExchRate,
-                        SalesAmt = suppliersHotelsInfo.LCAP,
-                        SupplierID = long.Parse(suppliersHotelsInfo.SupplierID.ToString()),
-                        SupplierRefNo = array[0].ToString().Trim(),
-                        SupplierConfNo = array[1].ToString().Trim(),
-                        PayCurrID = suppliersHotelsInfo.CurrencyCode,
-                        PayExRate = suppliersHotelsInfo.ExchRate,
-                        PayAmt = suppliersHotelsInfo.FCAP,
-                        ServiceName = DbParameter.GetServiceName(1),
-                        CheckIN = x.fromDate,
-                        checkOUT = x.toDate,
-                        NoOfNights = suppliersHotelsInfo.NumOfNights,
-                        ProductID = suppliersHotelsInfo.LocHotelID,
-                        LeadPaxName = txtFirstName.Text + " " + txtMiddleName.Text + " " + txtLastName.Text,
-                        NoOfRooms = 1,
-                        Adults = x.guestDetails.Where(y => y.type == "Adult").Count(),
-                        Children = x.guestDetails.Where(y => y.type == "Kid").Count(),
-                        Infants = x.guestDetails.Where(y => y.type == "Infant").Count(),
-                        HasExtraPaxs = true,
-                        RateSDID = suppliersHotelsInfo.RateSDID,
-                        supplierHotelID = int.Parse(suppliersHotelsInfo.HotelID.ToString()),
-                        StarsLevel = suppliersHotelsInfo.StarsLevel,
-                        SupplierRoomID = suppliersHotelsInfo.RoomID,
-                        SupplierRoomName = suppliersHotelsInfo.RoomName,
-                        SupplierRoomType = suppliersHotelsInfo.RoomType,
-                        SupplierRoomTypeID = suppliersHotelsInfo.RoomTypeID,
-                        BBID = suppliersHotelsInfo.BBID,
-                        BBName = suppliersHotelsInfo.BBName,
-                        ModDate = DateTime.Now,
-                        UserID = CurrentUser.Id().ToString()
-                    };
-                    _meis007Entities.AddToBkgMasters(bkgMaster);
-                    _meis007Entities.SaveChanges();
-                    foreach (var y in x.guestDetails)
-                    {
-                        paxDetail = new PaxDetail
-                        {
-                            BkgID = bkgMaster.BkgID,
-                            PaxName = y.firstName + " " + y.middleName + " " + y.lastName
-                        };
-                        _meis007Entities.AddToPaxDetails(paxDetail);
-                        _meis007Entities.SaveChanges();
-                    }
-                    BkgCancelPolicyHelper bkgCancelPolicyHelper =
-                        new BkgCancelPolicyHelper {
-                            _meis007Entities = _meis007Entities,
-                           bkgMaster = bkgMaster,
-                           EndDate = x.toDate.ToString(),
-                           hotelBooking = obj,
-                           StartDate = x.fromDate.ToString(),
-                           SupplierName = x.SupplierName,
-                           suppliersHotelsInfo = suppliersHotelsInfo
-                        };
-                    bkgCancelPolicyHelper.CreateBkgCancelPolicies();
+                    localBooking = new LocalBooking(_meis007Entities, suppliersHotelsInfo, x, obj, basketSequence, array[0].ToString().Trim(), array[1].ToString().Trim(), name, "Hotel");
+                    localBooking.Create();
                     hotelsToRemove.Add(x);
                 }
             }
-        }
-        if (bookedHotels) {
-            sequence.SequenceNumber = basketSequence;
-            _meis007Entities.SaveChanges();
         }
         RemoveHotels(basketHelper, hotelsToRemove);
         UpdateBasketHelperObject(basketHelper);
         Session["NoticeMessage"] = "Successfully confirmed bookings!";
         Response.Redirect(Route.GetRootPath("basket/show.aspx"));
-    }
-
-    protected string CreateBooking(BasketHotelDetails x, SuppliersHotelsInfo suppliersHotelsInfo) {
-        BookingDetails bookingInfo = new BookingDetails();
-        bookingInfo.CheckIn = x.fromDate;
-        bookingInfo.CheckOut = x.toDate;
-        bookingInfo.AdultNum = x.guestDetails.Where(y => y.type == "Adult").Count();
-        bookingInfo.ChildNum = x.guestDetails.Where(y => y.type == "Kid").Count();
-        bookingInfo.ChildAges = x.guestDetails.Where(y => y.type == "Kid").Select(y => int.Parse(y.age)).ToArray();
-        bookingInfo.AgentNumber = x.hotelInfoId.ToString();
-        bookingInfo.FirstName = txtFirstName.Text;
-        bookingInfo.MiddleName = txtMiddleName.Text;
-        bookingInfo.LastName = txtLastName.Text;
-        bookingInfo.MobilePhone = txtMobile.Text;
-        bookingInfo.HomePhone = txtTelephone.Text;
-        bookingInfo.HotelId = int.Parse(suppliersHotelsInfo.HotelID.ToString());
-        bookingInfo.RoomId = int.Parse(suppliersHotelsInfo.RoomID.ToString());
-        bookingInfo.RoomTypeId = int.Parse(suppliersHotelsInfo.RoomTypeID.ToString());
-        bookingInfo.Supplier = DbParameter.GetSupplierName(suppliersHotelsInfo.SupplierID);
-        bookingInfo.IsAvailable = true;
-        bookingInfo.RequestedPrice = decimal.Parse((suppliersHotelsInfo.AvrNightPricePay * suppliersHotelsInfo.NumOfNights).ToString());
-        bookingInfo.DeltaPrice = 1;
-        bookingInfo.Currency = suppliersHotelsInfo.CurrencyCode;
-        bookingInfo.SessionId = suppliersHotelsInfo.SessionID;
-        bookingInfo.SearchId = int.Parse(suppliersHotelsInfo.SearchID.ToString());
-        bookingInfo.SupplierId = suppliersHotelsInfo.SupplierID;
-        bookingInfo.CustomerId = "1015"; //DbParameter.GetCustomerId();
-        bookingInfo.HotelInfoId = int.Parse(x.hotelInfoId.ToString());
-        RepositoryFactory supplierFactory = new RepositoryFactory(null, x.sessionId);
-        return supplierFactory.HotelRoomBooking(bookingInfo);
     }
 
     protected void RemoveHotels(BasketHelper basketHelper, List<BasketHotelDetails> hotelsToRemove)
